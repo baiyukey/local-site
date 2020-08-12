d5db56c5b5e39538acac0c6cb95a4dc39adfb26a
baiyukey <baiyukey@hotmail.com>
2020-08-13 03:47:02 +0800

    [优化]模块，安装不必依赖JAVA

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let localSite=require("./lib/index.js");//local-site不在node_module中时
 let minifyProject=require("./lib/minifyProject.js");
 let arguments=process.argv.slice(2);
 if(arguments.length===0) console.log("您可选择的参数有：-service | -minify");
-else if (arguments[0]==="-service") localSite.run();
-else if(arguments[0]==="-minify") minifyProject.run();
+else if (arguments[0]==="-service") localSite.run();//启动local-site服务
+else if(arguments[0]==="-minify") minifyProject.run();//整个项目强制minify
